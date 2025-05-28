9681771387a0ae94cad48db2bf75a1bf6bb666cf
Tim <tim.grobecker111@gmail.com>
2025-05-28 15:27:02 +0200

    upload all files

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+// PascalCasing = Capitalize Each Letter Of The First Word, it's a common convention
+function Message() {
+    // JSX: JavaScript XML
+    const name = 'Tim';
+    if (name)
+        return <h1>Hello {name}</h1>;   
+    return <h1>Hello World</h1>
+}
+
+export default Message;
